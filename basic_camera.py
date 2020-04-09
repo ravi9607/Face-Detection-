0f39dbb27210bf7d1606106d870324ab8934d9f8
Ravi Gupta <61700885+ravi9607@users.noreply.github.com>
2020-04-09 20:41:11 +0530

    Update basic_camera.py

--- basic_camera.py
+++ basic_camera.py
@@ -23,7 +23,8 @@ while True:
         print(x, y, w, h)
 
     cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
         break
 
 cap.release()
