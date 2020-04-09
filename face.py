94be66f4843fae66fec49f3af0eccea8bff27adb
Ravi Gupta <61700885+ravi9607@users.noreply.github.com>
2020-04-09 20:36:43 +0530

    Add files via upload

---
+++ face.py
@@ -0,0 +1,36 @@
+import cv2
+
+# Load the cascade
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    #faces = faceCascade.detectMultiScale(gray,1.1, 5)
+    faces = faceCascade.detectMultiScale(gray,1.1, 5)
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display
+    cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+        
+# Release 
+cap.release()
+cv2.destroyAllWindows()
